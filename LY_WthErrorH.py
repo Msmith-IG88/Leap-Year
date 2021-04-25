3c88f67d6b06cb6a02a7079ab2f45bc92c94707b
Michael <smitmic5@oregonstate.edu>
2021-04-24 18:37:46 -0700

    Update comments

--- LY_WthErrorH.py
+++ LY_WthErrorH.py
@@ -4,12 +4,6 @@ Date: April 2021
 Description:
 This program takes an input from the user and checks
 to see if that input is a leapyear. 
-Instructions:
-1. Navigate to the directory this python file is in.
-2. Enter "python3 LY_WthErrorH.py" on the commandline, 
-this should start the program.
-3. The software will ask you to enter a year and output the results 
-all to the commandline.
 """
 def leapYear():
     year = input("Enter a year: ")
